893272bdf3db7c535533c74e375b6c6723927ce8
Fill <filonchenko@gmail.com>
2015-01-08 13:24:11 +0200

    AuthorizedUserPage

--- src/test/java/FunctionalTests/Pages/AuthorizedUserPage.java
+++ src/test/java/FunctionalTests/Pages/AuthorizedUserPage.java
@@ -7,14 +7,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 // * Created for W-xmlm by Fill on 05.01.2015.
 public class AuthorizedUserPage {
-    private static final By home = By.xpath("//div[@id='head-nav']//a[@href='/']");
-    private static final By money = By.xpath("//div[@id='head-nav']//a[@href='/money/']");
-/*
+    private static final By home = By.xpath("//div[@id='head-nav']//li[4]/a[@href='/']");
+    private static final By money = By.xpath("//div[@id='head-nav']//li[8]/a[@href='/money/']");
     public static final By headNav = By.xpath("//div[@id='head-nav']");
-*/
-    public static final By products = By.xpath("//div[@id='head-nav']//visible-lg//a[@href='/products/']");
-    private static final By net = By.xpath("//div[@id='head-nav']//a[@href='/network/']");
-    private static final By career = By.xpath("//div[@id='head-nav']//a[@href='/career/']");
+    public static final By products = By.xpath("//div[@id='head-nav']//li[12]/a[@href='/products/']");
+    private static final By net = By.xpath("//div[@id='head-nav']//li[16]/a[@href='/network/']");
+    private static final By career = By.xpath("//div[@id='head-nav']//li[20]/a[@href='/career/']");
     private static final By profilePage = By.xpath("//img[@alt='Avatar']");
     private static final By userMenu = By.xpath("//li[@class='dropdown']/a[@class='dropdown-toggle']");
     private static final By mySettings = By.linkText("//a[text()='My Settings']");
