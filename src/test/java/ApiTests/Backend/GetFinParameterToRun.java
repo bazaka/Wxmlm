ee4d50e0b8e2e099515fa1a7bbcf41cdc573d6fc
User <yura.bazaka@gmail.com>
2015-04-08 17:45:38 +0300

    New changelist

---
+++ src/test/java/ApiTests/Backend/GetFinParameterToRun.java
@@ -0,0 +1,130 @@
+package ApiTests.Backend;
+
+import ApiTests.ObjectClasses.Card;
+import ApiTests.ObjectClasses.FinParameter;
+import ApiTests.UsedByAll.MakeRequest;
+import ApiTests.UsedByAll.ValidationChecker;
+import UsedByAll.Config;
+import UsedByAll.CsvUsersReader;
+import UsedByAll.TestUser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by User on 4/8/2015.
+ */
+@RunWith(value= Parameterized.class)
+public class GetFinParameterToRun {
+    private TestUser testUser;
+
+    @Parameterized.Parameters
+    public static Collection testData() {
+        return CsvUsersReader.getDataForTest("_BackendAPITest(");
+    }
+
+    public GetFinParameterToRun(TestUser user) {
+        this.testUser = user;
+    }
+    @Test
+    public void testGetFinParameters() throws  Exception{
+        String siteUrl = Config.getConfig().getProtocol() + Config.getConfig().getScheme(); // Урл проверяемого сайта
+        long startTime;
+        long elapsedTime;
+        startTime = System.currentTimeMillis();
+        HttpURLConnection httpCon = MakeRequest.getConnection(siteUrl, testUser, "money/api/parameter/", 500, "GET");
+        InputStream inStrm = httpCon.getInputStream();
+        assertTrue("Check response code is 200", httpCon.getResponseCode() == 200);
+        elapsedTime = System.currentTimeMillis() - startTime;
+        InputStreamReader isReader = new InputStreamReader(inStrm);
+        BufferedReader br = new BufferedReader(isReader);
+        String result = "";
+        String line;
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+        br.close();
+        //Парсим JSON
+        JSONArray jsonArr = new JSONArray(result);
+        //Проверяем структуру
+        assertNotSame("Получен пустой массив. Рекомендуется проверить метод с наличием объектов. ", jsonArr.length(), 0);
+        for (int i = 0; i < jsonArr.length(); i++) {
+            JSONObject object = jsonArr.getJSONObject(i);
+
+            assertTrue("Incorrect id", ValidationChecker.checkIdValue(object.getInt("id")));
+            assertTrue("Incorrect operation_type_id", ValidationChecker.checkIdValue(object.getInt("operation_type_id")));
+            assertTrue("Incorrect merchant_id", ValidationChecker.checkIdOrNull(object.get("merchant_id")));
+            assertTrue("Incorrect payment_merchant_id", ValidationChecker.checkIdOrNull(object.get("payment_merchant_id")));
+            assertTrue("Incorrect parameter_id", ValidationChecker.checkIdValue(object.getInt("parameter_id")));
+            assertTrue("Incorrect value", ValidationChecker.checkStringNotNull(object.getString("value")));
+            assertTrue("Incorrect date_start", ValidationChecker.checkDateTimeString(object.get("date_start").toString()));
+            assertTrue("Incorrect date_end", ValidationChecker.checkDateTimeOrNull(object.get("date_end")));
+            assertTrue("Incorrect enabled", ValidationChecker.checkBooleanValue(object.getBoolean("enabled")));
+
+            assertEquals("Incorrect count of JSON parameters", object.length(), 9);
+        }
+        System.out.println("Total elapsed http request/response time in milliseconds: " + elapsedTime);
+    }
+    public FinParameter getAnyFinParameter(TestUser user, String siteUrl) throws IOException{
+        HttpURLConnection httpCon = MakeRequest.getConnection(siteUrl, user, "money/api/parameter/", 500, "GET");
+        InputStream inStrm = httpCon.getInputStream();
+        assertTrue("Check response code is 200", httpCon.getResponseCode() == 200);
+        InputStreamReader isReader = new InputStreamReader(inStrm);
+        BufferedReader br = new BufferedReader(isReader);
+        String result = "";
+        String line;
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+        br.close();
+        try {
+            JSONArray jsonArr = new JSONArray(result);
+            JSONObject object = jsonArr.getJSONObject(0);
+            // System.out.println(object);
+            return new FinParameter(object.getInt("id"),object.getInt("operation_type_id"), object.get("merchant_id"), object.get("payment_merchant_id"), object.getInt("parameter_id"),object.getString("value"),object.getString("date_start"),object.get("date_end"),object.getBoolean("enabled")  );
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public FinParameter getFinParameterByParameter(String parameterName, int parameterValue, TestUser user, String siteUrl) throws IOException {
+        HttpURLConnection httpCon = MakeRequest.getConnection(siteUrl, user, "money/api/parameter/", 1, "GET");
+        InputStream inStrm = httpCon.getInputStream();
+        assertTrue("Check response code is 200", httpCon.getResponseCode() == 200);
+        InputStreamReader isReader = new InputStreamReader(inStrm);
+        BufferedReader br = new BufferedReader(isReader);
+        String result = "";
+        String line;
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+        br.close();
+        try {
+            JSONArray jsonArr = new JSONArray(result);
+            for (int i = 0; i < jsonArr.length(); i++) {
+                JSONObject object = jsonArr.getJSONObject(i);
+                if (object.getInt(parameterName) == parameterValue) {
+                    return new FinParameter(object.getInt("id"),object.getInt("operation_type_id"), object.get("merchant_id"), object.get("payment_merchant_id"), object.getInt("parameter_id"),object.getString("value"),object.getString("date_start"),object.get("date_end"),object.getBoolean("enabled")  );
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ApiTests/Backend/PostFinParameterInsertToRun.java
@@ -0,0 +1,77 @@
+package ApiTests.Backend;
+
+import ApiTests.ObjectClasses.FinParameter;
+import ApiTests.UsedByAll.MakeRequest;
+import UsedByAll.Config;
+import UsedByAll.CsvUsersReader;
+import UsedByAll.TestUser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.util.Collection;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by User on 4/8/2015.
+ */
+@RunWith(value = Parameterized.class)
+public class PostFinParameterInsertToRun {
+    private TestUser testUser;
+
+    @Parameterized.Parameters
+    public static Collection testData() {
+        return CsvUsersReader.getDataForTest("_BackendAPITest(");
+    }
+
+    public PostFinParameterInsertToRun(TestUser user) {
+        this.testUser = user;
+    }
+
+    @Test
+    public void testPostFinParameterInsert() throws IOException, JSONException {
+        String siteUrl = Config.getConfig().getProtocol() + Config.getConfig().getScheme(); // Урл проверяемого сайта
+        long startTime;
+        long elapsedTime;
+        FinParameter originalOne = new GetFinParameterToRun(testUser).getAnyFinParameter(testUser, siteUrl);
+        FinParameter newOne = new FinParameter(originalOne.getId(), originalOne.getOperationTypeId(), originalOne.getMerchantId(), originalOne.getPaymentMerchantId(), originalOne.getParameterId(), originalOne.getValue(), originalOne.getDateStart(), originalOne.getDateEnd(), originalOne.getEnabled());
+
+
+
+        String newJson = "{\"operation_type_id\":"+newOne.getOperationTypeId() +", \"merchant_id\":" +newOne.getMerchantId()+", \"payment_merchant_id\":"+newOne.getPaymentMerchantId()+", \"parameter_id\":"+newOne.getParameterId()+", \"value\":\"" + newOne.getValue()+"\", \"date_start\":\""+newOne.getDateStart()+"\", \"date_end\":\""+newOne.getDateEnd()+"\", \"enabled\":"+newOne.getEnabled()+"}";
+
+        startTime = System.currentTimeMillis();
+        HttpURLConnection httpCon = MakeRequest.getConnection(siteUrl, testUser, "money/api/parameter/insert/", "POST", "application/json", "application/json", true);
+        OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
+        out.write(newJson);
+        out.close();
+        InputStream inStrm = httpCon.getInputStream();
+        assertTrue("Check response code is 200", httpCon.getResponseCode() == 200);
+        elapsedTime = System.currentTimeMillis() - startTime;
+        InputStreamReader isReader = new InputStreamReader(inStrm);
+        BufferedReader br = new BufferedReader(isReader);
+        String result = "";
+        String line;
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+        br.close();
+
+        // Берем из респонса id новой операции
+        JSONObject response = new JSONObject(result);
+        JSONArray report = response.getJSONArray("reports");
+        String parseResponse = report.getString(0).split("ID ")[1];
+        newOne.setId(Integer.parseInt(parseResponse.split(" was")[0]));
+
+        // Проверяем GET-запросом, что данные обновились
+        FinParameter changedOne = new GetFinParameterToRun(testUser).getFinParameterByParameter("id", newOne.getId(), testUser, siteUrl);
+        assertTrue("Check modified data saved correctly", newOne.equalsFin(changedOne));
+        System.out.println("Total elapsed http request/response time in milliseconds: " + elapsedTime);
+    }
+}
